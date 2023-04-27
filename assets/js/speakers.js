789914346abc6d6beb81c16987d5924119d1f792
diarisdiakite <diarisdiakite@gmail.com>
2023-04-27 07:29:24 +0200

    Add participants class content through javascript.

--- assets/js/speakers.js
+++ assets/js/speakers.js
@@ -1,17 +1,19 @@
-import { speakers } from './list.js';
+import { speakers, participants } from './list.js';
 
-console.log(speakers);
+//console.log(speakers);
 
 const speakersContainer = document.getElementById('speakers');
 
 speakers.forEach((speaker) => {
   speakersContainer.innerHTML += `
-    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 mt-5">
+    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
     <!-- Speaker card-->
       <div class="row">
+        
         <div class="col-xs-4 col-sm-4 col-md-4 col-xl-4 text-center" id="speaker-img-container">
-          <img id="speakerImg" class="w-100 h-75 align-middle" src="${speaker.mainImageUrl}" alt="">
+          <img id="speakerImg" class="w-100 h-50 align-middle mt-5" src="${speaker.mainImageUrl}" alt="">
         </div>
+        
         <div class="col-xs-8 col-sm-8 col-md-8 col-xl-8">
           <h5 class="bold">
             ${speaker.name}
@@ -33,3 +35,17 @@ speakers.forEach((speaker) => {
 });
 
 // speakersContainer += speakerCardContent;
+
+const participantsContainer = document.getElementById('participantsJs');
+
+participants.forEach((participant) => {
+  participantsContainer.innerHTML += `
+    <div class="col-xs-6 col-sm-6 col-md-4 col-xl-2 item3">
+      <img class="img-thumbnail img-fluid" id="participants-img" src="${participant.mainImageUrl}" alt="">
+      <div class="participants-img-caption text-center card-img-overlay">
+        <p class="bold my-bg-light w-75 mx-auto">${participant.name}</p>
+      </div>
+    </div>
+    
+    `
+});
