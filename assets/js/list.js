b01d8282caa88f2e556485ad0dca4b666c6d072c
diarisdiakite <diarisdiakite@gmail.com>
2023-04-27 07:28:30 +0200

    Add participants list

--- assets/js/list.js
+++ assets/js/list.js
@@ -26,19 +26,7 @@ export const speakers = [
   {
     status: 'speaker',
     mainImageUrl: 'assets/img/speakers/2.jpeg',
-    name: 'Axel',
-    position: 'Lorem ipsum dolor sit amet',
-    presentation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor',
-    products: [
-      1,
-      2,
-      3,
-    ],
-  },
-  {
-    status: 'speaker',
-    mainImageUrl: 'assets/img/speakers/3.jpg',
-    name: 'Quincy Larson',
+    name: 'Ariel',
     position: 'Lorem ipsum dolor sit amet',
     presentation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor',
     products: [
@@ -51,18 +39,6 @@ export const speakers = [
     status: 'speaker',
     mainImageUrl: 'assets/img/speakers/4.jpeg',
     name: 'Romans',
-    position: 'Lorem ipsum dolor sit amet',
-    presentation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor',
-    products: [
-      1,
-      2,
-      3,
-    ],
-  },
-  {
-    status: 'speaker',
-    mainImageUrl: 'assets/img/speakers/5.jpeg',
-    name: 'Bee',
     position: 'Lorem ipsum dolor sit amet',
     presentation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor',
     products: [
@@ -100,8 +76,8 @@ export const speakers = [
 export const participants = [
   {
     status: 'participant',
-    mainImageUrl: 'assets/img/speakers/1.jpeg', // to change
-    name: 'ABCD Center',
+    mainImageUrl: 'assets/img/participants/1.jpeg', // to change
+    name: '@Participant1',
     position: 'Agro Industry',
     presentation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor',
     mainProducts: [
@@ -112,8 +88,8 @@ export const participants = [
   },
   {
     status: 'participant',
-    mainImageUrl: 'assets/img/speakers/7.jpeg', // To change
-    name: 'Dr Awele',
+    mainImageUrl: 'assets/img/participants/2.png', // To change
+    name: '@Participant2',
     position: 'Lorem ipsum dolor sit amet',
     presentation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor',
     products: [
@@ -123,9 +99,9 @@ export const participants = [
     ],
   },
   {
-    status: 'speaker',
-    mainImageUrl: 'assets/img/speakers/2.jpeg',
-    name: 'Axel',
+    status: 'participant',
+    mainImageUrl: 'assets/img/participants/3.webp',
+    name: '@Participant3',
     position: 'Lorem ipsum dolor sit amet',
     presentation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor',
     products: [
@@ -135,9 +111,9 @@ export const participants = [
     ],
   },
   {
-    status: 'speaker',
-    mainImageUrl: 'assets/img/speakers/3.jpg',
-    name: 'Quincy Larson',
+    status: 'participant',
+    mainImageUrl: 'assets/img/participants/4.jpeg',
+    name: '@Participant4',
     position: 'Lorem ipsum dolor sit amet',
     presentation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor',
     products: [
@@ -147,9 +123,9 @@ export const participants = [
     ],
   },
   {
-    status: 'speaker',
-    mainImageUrl: 'assets/img/speakers/4.jpeg',
-    name: 'Romans',
+    status: 'participant',
+    mainImageUrl: 'assets/img/participants/5.jpeg',
+    name: '@Participant5',
     position: 'Lorem ipsum dolor sit amet',
     presentation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor',
     products: [
@@ -159,9 +135,9 @@ export const participants = [
     ],
   },
   {
-    status: 'speaker',
-    mainImageUrl: 'assets/img/speakers/5.jpeg',
-    name: 'Bee',
+    status: 'participant',
+    mainImageUrl: 'assets/img/participants/6.jpeg',
+    name: '@Participant6',
     position: 'Lorem ipsum dolor sit amet',
     presentation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor',
     products: [
@@ -171,9 +147,9 @@ export const participants = [
     ],
   },
   {
-    status: 'speaker',
-    mainImageUrl: 'assets/img/speakers/6.jpg',
-    name: 'Lincoln',
+    status: 'participant',
+    mainImageUrl: 'assets/img/participants/7.jpeg',
+    name: '@Participant7',
     position: 'Lorem ipsum dolor sit amet',
     presentation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor',
     products: [
@@ -183,9 +159,201 @@ export const participants = [
     ],
   },
   {
-    status: 'speaker',
-    mainImageUrl: 'assets/img/speakers/8.jpeg',
-    name: 'Adam',
+    status: 'participant',
+    mainImageUrl: 'assets/img/participants/8.jpeg',
+    name: '@Participant8',
+    position: 'Lorem ipsum dolor sit amet',
+    presentation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor',
+    products: [
+      1,
+      2,
+      3,
+    ],
+  },
+  {
+    status: 'participant',
+    mainImageUrl: 'assets/img/participants/9.jpeg',
+    name: '@Participant9',
+    position: 'Lorem ipsum dolor sit amet',
+    presentation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor',
+    products: [
+      1,
+      2,
+      3,
+    ],
+  },
+  {
+    status: 'participant',
+    mainImageUrl: 'assets/img/participants/10.jpeg',
+    name: '@Participant10',
+    position: 'Lorem ipsum dolor sit amet',
+    presentation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor',
+    products: [
+      1,
+      2,
+      3,
+    ],
+  },
+  {
+    status: 'participant',
+    mainImageUrl: 'assets/img/participants/11.jpg',
+    name: '@Participant11',
+    position: 'Lorem ipsum dolor sit amet',
+    presentation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor',
+    products: [
+      1,
+      2,
+      3,
+    ],
+  },
+  {
+    status: 'participant',
+    mainImageUrl: 'assets/img/participants/12.jpeg',
+    name: '@Participant12',
+    position: 'Lorem ipsum dolor sit amet',
+    presentation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor',
+    products: [
+      1,
+      2,
+      3,
+    ],
+  },
+  {
+    status: 'participant',
+    mainImageUrl: 'assets/img/participants/13.jpeg',
+    name: '@Participant13',
+    position: 'Lorem ipsum dolor sit amet',
+    presentation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor',
+    products: [
+      1,
+      2,
+      3,
+    ],
+  },
+  {
+    status: 'participant',
+    mainImageUrl: 'assets/img/participants/14.jpg',
+    name: '@Participant14',
+    position: 'Lorem ipsum dolor sit amet',
+    presentation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor',
+    products: [
+      1,
+      2,
+      3,
+    ],
+  },
+  {
+    status: 'participant',
+    mainImageUrl: 'assets/img/participants/15.jpeg',
+    name: '@Participant15',
+    position: 'Lorem ipsum dolor sit amet',
+    presentation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor',
+    products: [
+      1,
+      2,
+      3,
+    ],
+  },
+  {
+    status: 'participant',
+    mainImageUrl: 'assets/img/participants/16.jpeg',
+    name: '@Participant16',
+    position: 'Lorem ipsum dolor sit amet',
+    presentation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor',
+    products: [
+      1,
+      2,
+      3,
+    ],
+  },
+  {
+    status: 'participant',
+    mainImageUrl: 'assets/img/participants/17.jpeg',
+    name: '@Participant17',
+    position: 'Lorem ipsum dolor sit amet',
+    presentation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor',
+    products: [
+      1,
+      2,
+      3,
+    ],
+  },
+  {
+    status: 'participant',
+    mainImageUrl: 'assets/img/participants/18.jpeg',
+    name: '@Participant18',
+    position: 'Lorem ipsum dolor sit amet',
+    presentation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor',
+    products: [
+      1,
+      2,
+      3,
+    ],
+  },
+  {
+    status: 'participant',
+    mainImageUrl: 'assets/img/participants/19.jpeg',
+    name: '@Participant19',
+    position: 'Lorem ipsum dolor sit amet',
+    presentation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor',
+    products: [
+      1,
+      2,
+      3,
+    ],
+  },
+  {
+    status: 'participant',
+    mainImageUrl: 'assets/img/participants/20.jpg',
+    name: '@Participant20',
+    position: 'Lorem ipsum dolor sit amet',
+    presentation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor',
+    products: [
+      1,
+      2,
+      3,
+    ],
+  },
+  {
+    status: 'participant',
+    mainImageUrl: 'assets/img/participants/21.jpeg',
+    name: '@Participant21',
+    position: 'Lorem ipsum dolor sit amet',
+    presentation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor',
+    products: [
+      1,
+      2,
+      3,
+    ],
+  },
+  {
+    status: 'participant',
+    mainImageUrl: 'assets/img/participants/22.jpg',
+    name: '@Participant22',
+    position: 'Lorem ipsum dolor sit amet',
+    presentation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor',
+    products: [
+      1,
+      2,
+      3,
+    ],
+  },
+  {
+    status: 'participant',
+    mainImageUrl: 'assets/img/participants/23.jpeg',
+    name: '@Participant23',
+    position: 'Lorem ipsum dolor sit amet',
+    presentation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor',
+    products: [
+      1,
+      2,
+      3,
+    ],
+  },
+  {
+    status: 'participant',
+    mainImageUrl: 'assets/img/participants/24.jpeg',
+    name: '@Participant24',
     position: 'Lorem ipsum dolor sit amet',
     presentation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor',
     products: [
